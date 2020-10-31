6765bb69e5afa7038d094231dc4fc40869b1ccee
Alan Victor <alan_victor_paulino@hotmail.com>
2020-10-30 21:36:57 -0300

    Create Xcode Project, and adjust the gitignore

---
+++ CatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatApp
+//
+//  Created by Alan Victor Paulino de Oliveira on 30/10/20.
+//  Copyright © 2020 Alan Paulino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
